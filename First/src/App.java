15984b1870babde718c6cab1230489622f38dcd1
PROGRAMMER : MR.  AMIT <167705391+Techhackontime999@users.noreply.github.com>
2024-08-11 13:02:53 +0530

    adding shotcut

--- First/src/App.java
+++ First/src/App.java
@@ -16,4 +16,5 @@ public class App {
 //java is run in two steps firstly compile then interprete it.
 // compile java file using[javac App.java] it convert java file in to bytecode and give App.class file
 // then java run using [java App] command
+// shortcut:- run and compile both are done in only one command [java App.java]
 // run java in repl(Read envaluate print loop) mode using [jshell] command in command prompt.
